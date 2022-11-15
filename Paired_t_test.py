5d5756d7a9c827ad2e375e0fc3167f629afa0e97
Drue Berry <118294388+drue-berry@users.noreply.github.com>
2022-11-15 11:57:47 -0500

    Add files via upload

---
+++ Paired_t_test.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from numpy import nan
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+data = input("Input file for t-testing:   ")
+location1 = str(input("Input first study location:   "))
+location2 = str(input("Input second study location:  "))
+metric1 = str(input("Input first study metric (generally Location)  "))
+metric2 = str(input("Input second study metric (Time, Weight, BP, etc)  "))
+
+df = pd.read_csv(data)
+
+print(df)
+
+first_location = df.loc[df [f"{metric1}"] == f"{location1}"]
+firstLocationList = list(first_location[f"{metric2}"])
+
+second_location = df.loc[df[f"{metric1}"] == f"{location2}"]
+secondLocationList = list(second_location[f"{metric2}"])
+
+print(stats.ttest_rel(firstLocationList, secondLocationList, axis=0, nan_policy='omit'))
+box_plot_data=[firstLocationList, secondLocationList]
+plt.boxplot(box_plot_data, )
+plt.show()
+    
+
+
